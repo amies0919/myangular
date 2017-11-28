fea27e176641fe752f05e36fbbd4836ea415b5ae
nobody0day <xyang.cq@gmail.com>
2017-11-28 15:13:11 +0800

    add Ternary Operator

--- src/parse.js
+++ src/parse.js
@@ -36,7 +36,7 @@ Lexer.prototype.lex = function (text) {
             this.readNumber();
         }else if(this.is('\'"')) {
             this.readString(this.ch);
-        }else if(this.is('[],{}:.()')){
+        }else if(this.is('[],{}:.()?')){
             this.tokens.push({
                 text: this.ch
             });
@@ -187,6 +187,7 @@ AST.AssignmentExpression = 'AssignmentExpression';
 AST.UnaryExpression = 'UnaryExpression';
 AST.BinaryExpression = 'BinaryExpression';
 AST.LogicalExpression = 'LogicalExpression';
+AST.ConditionalExpression = 'ConditionalExpression';
 AST.prototype.ast = function (text) {
     this.tokens = this.lexer.lex(text);
     return this.program();
@@ -204,9 +205,9 @@ AST.prototype.parseArguments = function () {
   return args;
 };
 AST.prototype.assignment = function () {
-    var left = this.logicalOR();
+    var left = this.ternary();
     if(this.expect('=')){
-        var right = this.logicalOR();
+        var right = this.ternary();
         return {type: AST.AssignmentExpression, left: left, right: right};
     }
     return left;
@@ -407,6 +408,22 @@ AST.prototype.logicalOR = function () {
   }
   return left;
 };
+AST.prototype.ternary = function () {
+  var test = this.logicalOR();
+  if(this.expect('?')){
+      var consequent = this.assignment();
+      if(this.consume(':')){
+          var alternate = this.assignment();
+          return {
+              type: AST.ConditionalExpression,
+              test: test,
+              consequent: consequent,
+              alternate: alternate
+          }
+      }
+  }
+  return test;
+};
 function ASTCompiler(astBuilder) {
     this.astBuilder = astBuilder;
 }
@@ -597,6 +614,13 @@ ASTCompiler.prototype.recurse = function (ast, context,create) {
             this.state.body.push(this.assign(intoId, this.recurse(ast.left)));
             this.if_(ast.operator === '&&'? intoId: this.not(intoId),
                 this.assign(intoId, this.recurse(ast.right)));
+            return intoId;
+        case AST.ConditionalExpression:
+            intoId = this.nextId();
+            var testId = this.nextId();
+            this.state.body.push(this.assign(testId, this.recurse(ast.test)));
+            this.if_(testId, this.assign(intoId, this.recurse(ast.consequent)));
+            this.if_(this.not(testId), this.assign(intoId, this.recurse(ast.alternate)));
             return intoId;
     }
 };
