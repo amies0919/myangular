bb4cbd8cf6e54d02feace7b7fd91b50c0e555063
nobody0day <xyang.cq@gmail.com>
2017-11-28 17:29:14 +0800

    add filtering with predicate functions

--- src/filter_filter.js
+++ src/filter_filter.js
@@ -1,7 +1,8 @@
 'use strict';
+var _ = require('lodash');
 function filterFilter(){
-    return function () {
-
+    return function (array, filterExpr) {
+        return _.filter(array, filterExpr);
     };
 }
 module.exports = filterFilter;
