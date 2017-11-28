61f18da210205b7835d8b7decaf9cb498ce6bdcf
nobody0day <xyang.cq@gmail.com>
2017-11-28 17:57:51 +0800

    add add deepCompare

--- src/filter_filter.js
+++ src/filter_filter.js
@@ -2,7 +2,34 @@
 var _ = require('lodash');
 function filterFilter(){
     return function (array, filterExpr) {
-        return _.filter(array, filterExpr);
+        var predicateFn;
+        if(_.isFunction(filterExpr)){
+            predicateFn = filterExpr;
+        }else if(_.isString(filterExpr)){
+            predicateFn = createPredicateFn(filterExpr);
+        }else {
+            return array;
+        }
+        return _.filter(array, predicateFn);
+    };
+}
+function createPredicateFn(expression) {
+    function deepCompare(actual,expected,comparator) {
+        if(_.isObject(actual)){
+            return _.some(actual, function (value) {
+                return comparator(value, expected);
+            });
+        } else {
+            return comparator(actual,expected);
+        }
+    }
+    function comparator(actual, expected) {
+        actual = actual.toLowerCase();
+        expected = expected.toLowerCase();
+        return actual.indexOf(expected) !== -1;
+    }
+    return function predicateFn(item) {
+        return deepCompare(item, expression, comparator);
     };
 }
 module.exports = filterFilter;
