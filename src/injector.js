1b8eb815cde42a5a13a78ca417330679ed8568b3
amies <amies.shen@gmail.com>
2017-12-07 22:55:20 +0800

    Injecting Dependencies to the  method

--- src/injector.js
+++ src/injector.js
@@ -13,7 +13,7 @@ function createInjector(modulesToLoad, strictDi) {
             cache[key] = value;
         },
         provider: function (key, provider) {
-            cache[key] = provider.$get();
+            cache[key] = invoke(provider.$get, provider);
         }
     };
     var FN_ARG = /^\s*(_?)(\S+)\1\s*$/;
