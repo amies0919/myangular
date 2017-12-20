9953de8d5377456e9685e17371965a78e82dbdaf
YangXin <xyang.cq@gmail.com>
2017-12-20 23:49:40 +0800

    creating Deferreds

--- src/q.js
+++ src/q.js
@@ -1,6 +1,16 @@
 'use strict';
 function $QProvider() {
     this.$get = function () {
+        function Deferred(){
+            
+        }
+        function defer() {
+            return new Deferred();
+        }
+
+        return {
+            defer:defer
+        };
         
     };
 }
