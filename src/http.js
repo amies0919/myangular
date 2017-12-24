902fd4dac452cc8a4b769f5a5c3cde2b203f3427
YangXin <xyang.cq@gmail.com>
2017-12-24 22:50:35 +0800

    request Headers

--- src/http.js
+++ src/http.js
@@ -1,12 +1,70 @@
 'use strict';
 var _ = require('lodash');
 function $HttpProvider() {
+    var defaults = this.defaults = {
+        headers: {
+            common: {
+                Accept: 'application/json, text/plain, */*'
+            },
+            post: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            put: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            patch: {
+                'Content-Type': 'application/json;charset=utf-8'
+            }
+        }
+    };
     this.$get = ['$httpBackend','$q', '$rootScope', function ($httpBackend, $q, $rootScope) {
-        return function $http(requestConfig){
+         function $http(requestConfig){
             var deferred = $q.defer();
             var config = _.extend({
                 method: 'GET'
             }, requestConfig);
+            config.headers = mergeHeaders(requestConfig);
+            if(_.isUndefined(config.data)){
+                _.forEach(config.headers, function (v,k) {
+                    if(k.toLowerCase() == 'content-type'){
+                        delete config.headers[k];
+                    }
+                });
+            }
+            function mergeHeaders(config) {
+                var reqHeaders = _.extend(
+                    {},
+                    config.headers
+                );
+                var defHeaders = _.extend(
+                    {},
+                    defaults.headers.common,
+                    defaults.headers[(config.method || 'get').toLowerCase()]
+                );
+                _.forEach(defHeaders, function (value, key) {
+                    var headerExists = _.some(reqHeaders, function (v, k) {
+                         return k.toLowerCase() == key.toLowerCase();
+                    });
+                    if(!headerExists){
+                        reqHeaders[key] = value;
+                    }
+                });
+                return executeHeaderFns(reqHeaders, config);
+
+            }
+             function executeHeaderFns(headers, config) {
+                 return _.transform(headers, function(result, v, k) {
+                     if (_.isFunction(v)) {
+                         v = v(config);
+                         if(_.isNull(v) || _.isUndefined(v)){
+                             delete result[k];
+                         }else{
+                             result[k] = v;
+                         }
+
+                     }
+                 }, headers);
+             }
             function done(status, response, statusText) {
                 status = Math.max(status, 0);
                 deferred[isSuccess(status)?'resolve':'reject']({
@@ -22,9 +80,17 @@ function $HttpProvider() {
             function isSuccess(status) {
                 return status>=200 && status<300;
             }
-            $httpBackend(config.method, config.url, config.data, done);
+            $httpBackend(
+                config.method,
+                config.url,
+                config.data,
+                done,
+                config.headers
+            );
             return deferred.promise;
-        };
+        }
+        $http.defaults = defaults;
+        return $http;
         
     }];
 }
