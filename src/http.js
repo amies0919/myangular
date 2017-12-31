835b43dd8e0eb312e16b9b5ca56e949d63fc625f
YangXin <xyang.cq@gmail.com>
2017-12-31 18:11:35 +0800

    response transforms

--- src/http.js
+++ src/http.js
@@ -18,67 +18,84 @@ function $HttpProvider() {
         }
     };
     this.$get = ['$httpBackend','$q', '$rootScope', function ($httpBackend, $q, $rootScope) {
-         function $http(requestConfig){
-            var deferred = $q.defer();
-            var config = _.extend({
-                method: 'GET',
-                transformRequest: defaults.transformRequest
-            }, requestConfig);
-            config.headers = mergeHeaders(requestConfig);
-            if(_.isUndefined(config.withCredentials) && !_.isUndefined(defaults.withCredentials)){
-                config.withCredentials = defaults.withCredentials;
-            }
-            var reqData = transformData(config.data, headersGetter(config.headers),config.transformRequest);
-            if(_.isUndefined(reqData)){
-                _.forEach(config.headers, function (v,k) {
-                    if(k.toLowerCase() == 'content-type'){
-                        delete config.headers[k];
-                    }
-                });
-            }
-            function transformData(data, headers , transform) {
-                if(_.isFunction(transform)){
-                    return transform(data, headers);
-                } else {
-                    return _.reduce(transform, function (data, fn) {
-                        return fn(data, headers);
-                    }, data);
+        function isSuccess(status) {
+            return status>=200 && status<300;
+        }
+        function headersGetter(headers) {
+            var headersObj;
+            return function (name) {
+                headersObj = headersObj || parseHeaders(headers);
+                if(name) {
+                    return headersObj[name.toLowerCase()];
+                }else{
+                    return headersObj;
                 }
-            }
-            function mergeHeaders(config) {
-                var reqHeaders = _.extend(
-                    {},
-                    config.headers
-                );
-                var defHeaders = _.extend(
-                    {},
-                    defaults.headers.common,
-                    defaults.headers[(config.method || 'get').toLowerCase()]
-                );
-                _.forEach(defHeaders, function (value, key) {
-                    var headerExists = _.some(reqHeaders, function (v, k) {
-                         return k.toLowerCase() == key.toLowerCase();
-                    });
-                    if(!headerExists){
-                        reqHeaders[key] = value;
+            };
+        }
+        function parseHeaders(headers) {
+            if(_.isObject(headers)){
+                return _.transform(headers, function (result, v, k) {
+                    result[_.trim(k.toLowerCase())] = _.trim(v);
+                }, {});
+
+            } else {
+                var lines = headers.split('\n');
+                return _.transform(lines, function (result, line) {
+                    var separatorAt = line.indexOf(':');
+                    var name = _.trim(line.substr(0, separatorAt)).toLowerCase();
+                    var value = _.trim(line.substr(separatorAt + 1));
+                    if (name) {
+                        result[name] = value;
                     }
-                });
-                return executeHeaderFns(reqHeaders, config);
 
+                }, {});
             }
-             function executeHeaderFns(headers, config) {
-                 return _.transform(headers, function(result, v, k) {
-                     if (_.isFunction(v)) {
-                         v = v(config);
-                         if(_.isNull(v) || _.isUndefined(v)){
-                             delete result[k];
-                         }else{
-                             result[k] = v;
-                         }
+        }
+        function executeHeaderFns(headers, config) {
+            return _.transform(headers, function(result, v, k) {
+                if (_.isFunction(v)) {
+                    v = v(config);
+                    if(_.isNull(v) || _.isUndefined(v)){
+                        delete result[k];
+                    }else{
+                        result[k] = v;
+                    }
 
-                     }
-                 }, headers);
-             }
+                }
+            }, headers);
+        }
+        function transformData(data, headers ,status , transform) {
+            if(_.isFunction(transform)){
+                return transform(data, headers, status);
+            } else {
+                return _.reduce(transform, function (data, fn) {
+                    return fn(data, headers, status);
+                }, data);
+            }
+        }
+        function mergeHeaders(config) {
+            var reqHeaders = _.extend(
+                {},
+                config.headers
+            );
+            var defHeaders = _.extend(
+                {},
+                defaults.headers.common,
+                defaults.headers[(config.method || 'get').toLowerCase()]
+            );
+            _.forEach(defHeaders, function (value, key) {
+                var headerExists = _.some(reqHeaders, function (v, k) {
+                    return k.toLowerCase() == key.toLowerCase();
+                });
+                if(!headerExists){
+                    reqHeaders[key] = value;
+                }
+            });
+            return executeHeaderFns(reqHeaders, config);
+
+        }
+        function sendReq(config, reqData) {
+            var deferred = $q.defer();
             function done(status, response, headersString,statusText) {
                 status = Math.max(status, 0);
                 deferred[isSuccess(status)?'resolve':'reject']({
@@ -92,39 +109,6 @@ function $HttpProvider() {
                     $rootScope.$apply();
                 }
             }
-            function headersGetter(headers) {
-                var headersObj;
-                return function (name) {
-                    headersObj = headersObj || parseHeaders(headers);
-                    if(name) {
-                        return headersObj[name.toLowerCase()];
-                    }else{
-                        return headersObj;
-                    }
-                };
-            }
-            function parseHeaders(headers) {
-                if(_.isObject(headers)){
-                    return _.transform(headers, function (result, v, k) {
-                        result[_.trim(k.toLowerCase())] = _.trim(v);
-                    }, {});
-
-                } else {
-                    var lines = headers.split('\n');
-                    return _.transform(lines, function (result, line) {
-                        var separatorAt = line.indexOf(':');
-                        var name = _.trim(line.substr(0, separatorAt)).toLowerCase();
-                        var value = _.trim(line.substr(separatorAt + 1));
-                        if (name) {
-                            result[name] = value;
-                        }
-
-                    }, {});
-                }
-            }
-            function isSuccess(status) {
-                return status>=200 && status<300;
-            }
             $httpBackend(
                 config.method,
                 config.url,
@@ -134,6 +118,38 @@ function $HttpProvider() {
                 config.withCredentials
             );
             return deferred.promise;
+        }
+         function $http(requestConfig){
+            var config = _.extend({
+                method: 'GET',
+                transformRequest: defaults.transformRequest,
+                transformResponse: defaults.transformResponse
+            }, requestConfig);
+            config.headers = mergeHeaders(requestConfig);
+            if(_.isUndefined(config.withCredentials) && !_.isUndefined(defaults.withCredentials)){
+                config.withCredentials = defaults.withCredentials;
+            }
+            var reqData = transformData(config.data, headersGetter(config.headers),undefined,config.transformRequest);
+            if(_.isUndefined(reqData)){
+                _.forEach(config.headers, function (v,k) {
+                    if(k.toLowerCase() == 'content-type'){
+                        delete config.headers[k];
+                    }
+                });
+            }
+            function transformResponse(response) {
+                if(response.data){
+                    response.data = transformData(response.data, response.headers,response.status, config.transformResponse)
+                }
+                if(isSuccess(response.status)) {
+                    return response;
+                } else {
+                    return $q.reject(response);
+                }
+                
+            }
+
+            return sendReq(config, reqData).then(transformResponse,transformResponse);
         }
         $http.defaults = defaults;
         return $http;
