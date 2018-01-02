f8addebebf29736456942f3683bec4992717cf0d
amies <amies.shen@gmail.com>
2018-01-02 20:52:29 +0800

    Manipulating Classes

--- src/compile.js
+++ src/compile.js
@@ -82,6 +82,24 @@ function $CompileProvider($provide) {
                     }
                 };
             };
+            Attributes.prototype.$addClass = function(classVal) {
+                this.$$element.addClass(classVal);
+            };
+            Attributes.prototype.$removeClass = function(classVal) {
+                this.$$element.removeClass(classVal);
+            };
+            Attributes.prototype.$updateClass = function(newClassVal, oldClassVal) {
+                var newClasses = newClassVal.split(/\s+/);
+                var oldClasses = oldClassVal.split(/\s+/);
+                var addedClasses = _.difference(newClasses, oldClasses);
+                var removedClasses = _.difference(oldClasses, newClasses);
+                if (addedClasses.length) {
+                    this.$addClass(addedClasses.join(' '));
+                }
+                if (removedClasses.length) {
+                    this.$removeClass(removedClasses.join(' '));
+                }
+            };
         }
         function compile($compileNodes) {
             return compileNodes($compileNodes);
@@ -208,9 +226,12 @@ function $CompileProvider($provide) {
                     }
                 }
             }else if(node.nodeType === Node.COMMENT_NODE) {
-                match = /^\s*directive\:\s*([\d\w\-_]+)/.exec(node.nodeValue);
+                match = /^\s*directive\:\s*([\d\w\-_]+)\s*(.*)$/.exec(node.nodeValue);
                 if (match) {
-                    addDirective(directives, directiveNormalize(match[1]), 'M');
+                    var normalizedName = directiveNormalize(match[1]);
+                    if(addDirective(directives, normalizedName, 'M')){
+                        attrs[normalizedName] = match[2] ? match[2].trim() : undefined;
+                    }
                 }
             }
             directives.sort(byPriority);
