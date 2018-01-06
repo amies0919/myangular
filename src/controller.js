ea5861e974b561aa66bf1c8e8ededd35a6a443bb
YangXin <xyang.cq@gmail.com>
2018-01-06 20:54:42 +0800

    controller registration

--- src/controller.js
+++ src/controller.js
@@ -1,7 +1,19 @@
 'use strict';
+var _ = require('lodash');
 function $ControllerProvider() {
+    var controllers = {};
+    this.register = function (name, controller) {
+        if(_.isObject(name)){
+            _.extend(controllers, name);
+        }else {
+            controllers[name] = controller;
+        }
+    };
     this.$get = ['$injector',function ($injector) {
         return function (ctrl, locals) {
+            if(_.isString(ctrl)){
+                ctrl = controllers[ctrl];
+            }
             return $injector.instantiate(ctrl, locals);
         };
         
