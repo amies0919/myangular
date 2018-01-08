d74dd4fa1e762bf09d7f29dde4791888a117bf37
amies <amies.shen@gmail.com>
2018-01-08 10:44:43 +0800

    global controller lookup

--- test/controller_spec.js
+++ test/controller_spec.js
@@ -79,4 +79,22 @@ describe('$controller', function () {
         var controller = $controller('MyController');
         expect(controller).toBeDefined();
    });
+    it('does not normally look controllers up from window', function() {
+        window.MyController = function MyController() { };
+        var injector = createInjector(['ng']);
+        var $controller = injector.get('$controller');
+        expect(function() {
+            $controller('MyController');
+        }).toThrow();
+    });
+    it('looks up controllers from window when so confgured', function() {
+        window.MyController = function MyController() { };
+        var injector = createInjector(['ng', function($controllerProvider) {
+            $controllerProvider.allowGlobals();
+        }]);
+        var $controller = injector.get('$controller');
+        var controller = $controller('MyController');
+        expect(controller).toBeDefined();
+        expect(controller instanceof window.MyController).toBe(true);
+    });
 });
