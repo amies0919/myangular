4006b3ad9fd2c47926c61d736fa74d40b9a04c16
amies <amies.shen@gmail.com>
2018-01-15 18:50:07 +0800

    the  service

--- src/angular_public.js
+++ src/angular_public.js
@@ -14,6 +14,7 @@ function publishExternalAPI() {
     ngModule.provider('$HttpParamSerializerJQLike', require('./http').$HttpParamSerializerJQLikeProvider);
     ngModule.provider('$compile', require('./compile'));
     ngModule.provider('$controller', require('./controller'));
+    ngModule.provider('$interpolate', require('./interpolate'));
     ngModule.directive('ngController', require('./directives/ng_controller'));
     ngModule.directive('ngTransclude', require('./directives/ng_transclude'));
 }
---
+++ src/interpolate.js
@@ -0,0 +1,9 @@
+'use strict';
+function $InterpolateProvider() {
+    this.$get = function() {
+        function $interpolate() {
+        }
+        return $interpolate;
+    };
+}
+module.exports = $InterpolateProvider;
