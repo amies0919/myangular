a021414979d83dbbeface148b0057ec83e0d9a73
amies <amies.shen@gmail.com>
2018-01-16 10:36:25 +0800

    requiring from components

--- test/compile_spec.js
+++ test/compile_spec.js
@@ -4032,6 +4032,25 @@ describe('$compile', function() {
                     expect(el.find('div').text()).toEqual('Transclude me');
                 });
             });
+            it('may require other directive controllers', function() {
+                var secondControllerInstance;
+                var injector = createInjector(['ng', function($compileProvider) {
+                    $compileProvider.component('first', {
+                        controller: function() { }
+                    });
+                    $compileProvider.component('second', {
+                        require: {first: '^'},
+                        controller: function() {
+                            secondControllerInstance = this;
+                        }
+                    });
+                }]);
+                injector.invoke(function($compile, $rootScope) {
+                    var el = $('<first><second></second></first>');
+                    $compile(el)($rootScope);
+                    expect(secondControllerInstance.first).toBeDefined();
+                });
+            });
         });
     });
 
